3f46440014c6185fa027c71c9b4962227334be6d
Sumedin Bilalic <sumko87@gmail.com>
2022-04-09 09:38:21 +0200

    contact list in JS

---
+++ ex08/src/index.js
@@ -0,0 +1,52 @@
+var contacts = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "number": "12365495208",
+        "likes": ["C", "C++", "C#"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "number": "4568295761",
+        "likes": ["Python", "Swift", "R"]
+    },
+    {
+        "firstName": "Richard ",
+        "lastName": "Roe",
+        "number": "6938257149",
+        "likes": ["React", "Angular", "Vue"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Roe",
+        "number": "unknown",
+        "likes": ["JavaScript", "Node", "HTML & CSS"]
+    }
+    ];
+    function lookUpProfile(name, prop){
+        // Only change code below this line
+        var firstName = false;
+        var property = false;
+        for (var i = 0; i < contacts.length; i++) {
+            if (contacts[i].firstName === name) {
+                firstName = true;
+                if (contacts[i].hasOwnProperty(prop)) {
+                    property = true;
+                    return contacts[i][prop];
+                }
+            }
+        }
+        if (!firstName) {
+            return "No such contact";
+        } else {
+            return "No such property";
+        }
+        // Only change code above this line
+    }
+    console.log(lookUpProfile("John", "likes")); // Change this line
+    console.log(lookUpProfile("Jane", "lastName"));
+    console.log(lookUpProfile("Rob", "number"));
+    console.log(lookUpProfile("Rob", "JavaScript"));
+    console.log(lookUpProfile("John", "address")); 
+    module.exports = lookUpProfile;
